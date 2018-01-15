8684534ccd7b1dc0ee429f37cbb73a9195be3b98
Corey Ferguson <corey.t.ferguson@gmail.com>
2018-01-15 08:03:00 -0800

    hoursPerDay

--- src/HoursThisWeek/HoursThisWeekService.js
+++ src/HoursThisWeek/HoursThisWeekService.js
@@ -4,7 +4,7 @@ const moment = require('moment');
 class HoursThisWeekService {
 
   getDates(data) {
-    return data.map(s => moment(new Date(s)));
+    return data.map(s => moment(new Date(s)).utc());
   }
 
   filterThisWeek(dates, startTime) {
@@ -20,6 +20,17 @@ class HoursThisWeekService {
       }
     }
     return filtered;
+  }
+
+  hoursPerDay(dates) {
+    const hoursPerDay = [0, 0, 0, 0, 0, 0, 0];
+    for (let i=1; i<dates.length; i+=2) {
+      const start = dates[i-1];
+      const end = dates[i];
+      const hours = end.diff(start, 'hours', true);
+      hoursPerDay[end.day()] += hours;
+    }
+    return hoursPerDay;
   }
 
 }
